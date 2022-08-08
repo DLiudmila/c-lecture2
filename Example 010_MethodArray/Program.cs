0a964366a3cba1b489e488bc4206c6906f17ccef
Liudmila D <verliebtin@mail.ru>
2022-08-08 18:09:59 +0300

    lecture2, method, array

---
+++ Example 010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {5, 56, 7, 98, 45, 34, 19, 43, 85, 7};
+int n = array.Length;
+
+int find = 7;
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine(i);
+    }
+     i++;
+   
+}
+
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[10];
+
+a(array);
+b(array);
+
+void a(int[] collection)
+{
+    int Length = collection.Length;
+    int i = 0;
+    while (i < Length)
+    {
+       collection[i] = new Random().Next(1, 10);
+       i++;
+    }
+}
+
+void b(int[] collection)
+{
+    int Length = collection.Length;
+    int i = 0;
+    while (i < Length)
+    {
+        Console.WriteLine(collection[i]);
+        i++;
+    }
+}
